70e16120e656f04190edbf90cf995100fa148898
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-05-31 01:35:17 -0700

    Add support for both full and shortened youtu.be links.

--- background.js
+++ background.js
@@ -1,12 +1,26 @@
+// Converts given YouTube link to a YouTube Music link
+function youtubeToYoutubeMusic(lnk) {
+    let youtubeMusicBase = "https://music.youtube.com/watch?v="
+    if (lnk.match(/youtu.be/) != null) {
+        // http://youtu.be/uhY9Zxv1-oo
+        return youtubeMusicBase + lnk.match(/youtu.be\/(.+)/)[1];
+    } else {
+        // https://www.youtube.com/watch?v=sP-IX4mdnFY#t=1m29s
+        return youtubeMusicBase + lnk.match(/v=([a-zA-Z0-9\-]+)/)[1];
+    }
+}
+
+// Opens YouTube Music link in a new tab
 function launchYoutubeMusic(info, tab) {
     chrome.tabs.create({
-        url: info.linkUrl.replace('www', 'music')
+        url: youtubeToYoutubeMusic(info.linkUrl)
     });
 }
 
+// Create context menu only for Youtube links
 chrome.contextMenus.create({
     id: "openInYTM",
     title: "Open in YouTube Music",
     contexts: ["link"],
-    onclick: launchYoutubeMusic
+    onclick: launchYoutubeMusic,
 });
