815d111cb3033a64be335cc19bc5eefa759dbab2
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-05-31 00:44:37 -0700

    Clean up launchYoutubeMusic().

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 function launchYoutubeMusic(info, tab) {
-    lnk = info.linkUrl;
-    console.log("Opening link: " + lnk);
-    ytm_lnk = lnk.replace('www', 'music');
     chrome.tabs.create({
-        url: ytm_lnk
+        url: info.linkUrl.replace('www', 'music')
     });
 }
 
