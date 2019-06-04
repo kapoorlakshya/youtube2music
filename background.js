6bb94d2c4ad5c0f7989512862c3d45b5b84318d1
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-06-04 01:29:53 -0700

    Update metadata Regex to include timestamp and playlist params. And throw error if nothing matches.

--- background.js
+++ background.js
@@ -8,19 +8,36 @@ window.browser = (function () {
 // Converts given YouTube link to a YouTube Music link
 function youtubeToYoutubeMusic(lnk) {
     let youtubeMusicBase = "https://music.youtube.com/watch?v=";
+    let ytmLink = '';
 
-    // Extract video ID
-    if (lnk.match(/youtu.be/) != null) {
+    // Extract video ID & other params
+    if (lnk.match(/http(s)?:\/\/youtu\.be/) != null) {
         // http://youtu.be/uhY9Zxv1-oo
         // -> uhY9Zxv1-oo
-        return youtubeMusicBase + lnk.match(/youtu.be\/([a-zA-Z0-9\-_]+)/)[1];
+        // https://youtu.be/hggISFswKcw?t=27
+        // -> hggISFswKcw?t=27
+        ytmLink = youtubeMusicBase + lnk.match(/youtu.be\/([a-zA-Z0-9\-_?=]+)/)[1];
     } else if (lnk.match(/youtube.com/) != null) {
         // https://www.youtube.com/watch?v=sP-IX4mdnFY#t=1m29s
-        // -> sP-IX4mdnFY
-        return youtubeMusicBase + lnk.match(/v=([a-zA-Z0-9\-_]+)/)[1];
+        // -> sP-IX4mdnFY#t=1m29s
+        // https://www.youtube.com/watch?v=9JOu_l1dets&list=PLVL8S3lUHf0TT2SGhaJy5FREpv2rUx2i3
+        // -> 9JOu_l1dets&list=PLVL8S3lUHf0TT2SGhaJy5FREpv2rUx2i3
+        // https://www.youtube.com/attribution_link?a=ujJ0W91Plr8&u=%2Fwatch%3Fv%3DLTTt-ikVJSk%26feature%3Dshare
+        // -> v%3DLTTt-ikVJSk
+        ytmLink = youtubeMusicBase + lnk.match(/v(?:=|%3D)([a-zA-Z0-9\-_#?=]+(&list.+)?)/)[1];
+        ytmLink = ytmLink.replace("%3D", "=");
     } else {
         alert("Unsupported link: " + lnk);
     }
+
+    // Failed to capture any values from the links. Maybe the pattern changed?
+    if (ytmLink === youtubeMusicBase) {
+        alert(failMessage(lnk)); // Inform user
+        throw(failMessage(lnk)); // Exit
+    }
+
+    console.log("Opening YouTube Music link: " + ytmLink);
+    return ytmLink;
 }
 
 // Opens YouTube Music link in a new tab
@@ -45,3 +62,8 @@ browser.contextMenus.create({
     ],
     onclick: launchYoutubeMusic,
 });
+
+function failMessage(lnk) {
+    return "Failed to open link: " + lnk + `\n\nPlease report this failure on the GitHub repo:
+        \nhttps://github.com/kapoorlakshya/youtube2music/issues\n\nThank you!`;
+}
